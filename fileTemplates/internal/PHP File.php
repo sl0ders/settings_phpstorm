603a4991739e6cf0f41758a2a7ba3b891fe09af6
quentin sommesous <quentin.sommesous@numeric-wave.eu>
2020-07-17 14:03:16 +0200

    first commit

---
+++ fileTemplates/internal/PHP File.php
@@ -0,0 +1,14 @@
+<?php
+#parse("PHP File Header.php")
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>#[[$Title$]]#</title>
+</head>
+<body>
+#[[$END$]]#
+</body>
+</html>
